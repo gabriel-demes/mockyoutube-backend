3f8aad7b0a60e597f8d79c28d94d24b4720a7cfd
celestialheathen <jingchaoyu@hotmail.com>
2021-03-26 18:29:34 -0400

    Added info

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -2,6 +2,9 @@ Cloudinary.config do |config|
     config.cloud_name = "gd1063"   
     config.api_key = "395673214828923"   
     config.api_secret = "8liyVY7QEeFpd6kHHF9W-GRPgq8"
+    # config.cloud_name = "celestialheathen"   
+    # config.api_key = "786954993666981"   
+    # config.api_secret = "M0PKa-BHLrJsb7hQxqwEEioABDY-GRPgq8"
     config.secure = true    
     config.cdn_subdomain = true  
 end
