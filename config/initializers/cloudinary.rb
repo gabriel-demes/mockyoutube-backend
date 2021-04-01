a79bdbd670c3e05d75ef89b8f573a34e2c4d01fe
celestialheathen <jingchaoyu@hotmail.com>
2021-04-01 08:51:03 -0400

    Added env file

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,10 +1,7 @@
 Cloudinary.config do |config|    
     config.cloud_name = "gd1063"   
-    config.api_key = "395673214828923"   
-    config.api_secret = "8liyVY7QEeFpd6kHHF9W-GRPgq8"
-    # config.cloud_name = "celestialheathen"   
-    # config.api_key = "786954993666981"   
-    # config.api_secret = "M0PKa-BHLrJsb7hQxqwEEioABDY-GRPgq8"
+    config.api_key = ENV['CLOUDINARY_API_KEY']   
+    config.api_secret = ENV['CLOUDINARY_API_SECRET ']
     config.secure = true    
     config.cdn_subdomain = true  
 end
